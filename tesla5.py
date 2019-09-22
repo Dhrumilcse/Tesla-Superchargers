2414cb07952d156c713b1edc57a14fe106211c26
Dhrumil Patel <dhrumilcse14@gmail.com>
2019-09-22 17:18:52 -0400

    Update tesla5.py

--- tesla5.py
+++ tesla5.py
@@ -1,24 +1,21 @@
-#1.0 Import Library
+#Import Library
 import folium
-from folium.plugins import MarkerCluster
 import pandas as pd
 
 #Load Data
 data = pd.read_excel("tesla-supercharger-france.xlsx")
-lat = data['Ylatitude']
-lon = data['Xlongitude']
-station = data['n_station']
+latitude = data['Ylatitude']
+longitude = data['Xlongitude']
+station = data['ad_station']
 
 #Create base map
-map = folium.Map(location=[47.85183, 3.542802], zoom_start = 6, tiles = "cartodbpositron" )
-#marker_cluster = MarkerCluster().add_to(map)
+map = folium.Map(location=[48.754865,2.373034], zoom_start = 6, tiles = "cartodbpositron")
 
-#Plot Marker
-for lat, lon, station in zip(lat, lon, station):
-    #Using Custom Tesla Icon
+#Plot Marker with custom Icon
+for latitude, longitude, station in zip(latitude, longitude, station):
     icon_path = "tesla-iconn.png"
     icon = folium.features.CustomIcon(icon_image=icon_path ,icon_size=(30,30))
-    folium.Marker(location=[lat, lon], popup=str(station), icon = icon).add_to(map)
+    folium.Marker(location=[latitude, longitude], popup=str(station), icon=folium.Icon(color = 'green')).add_to(map)
 
 #Save the map
 map.save("map.html")
