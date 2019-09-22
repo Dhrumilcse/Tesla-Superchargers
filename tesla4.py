0c8ce6a56cd27876f781d17f7a6ad3655d0cdb69
Dhrumil Patel <dhrumilcse14@gmail.com>
2019-09-22 17:19:16 -0400

    Update tesla4.py

--- tesla4.py
+++ tesla4.py
@@ -4,16 +4,16 @@ import pandas as pd
 
 #Load Data
 data = pd.read_excel("tesla-supercharger-france.xlsx")
-lat = data['Ylatitude']
-lon = data['Xlongitude']
+latitude = data['Ylatitude']
+longitude = data['Xlongitude']
 station = data['ad_station']
 
 #Create base map
 map = folium.Map(location=[48.754865,2.373034], zoom_start = 6)
 
 #Plot Marker
-for lat, lon, station in zip(lat, lon, station):
-    folium.Marker(location=[lat, lon], popup=str(station), icon=folium.Icon(color = 'green')).add_to(map)
+for latitude, longitude, station in zip(latitude, longitude, station):
+    folium.Marker(location=[latitude, longitude], popup=str(station), icon=folium.Icon(color = 'green')).add_to(map)
 
 #Save the map
 map.save("map.html")
